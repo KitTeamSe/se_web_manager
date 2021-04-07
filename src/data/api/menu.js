e355fff6f59aca7c5b54087f0a53921b98314200
jihun1233 <leejihun6535@gmail.com>
2021-04-07 16:21:10 +0900

    [UPDATE] api/menu.js 함수명에 api 다시 추가
    - 액션생성함수와 중복이 커서 api를 붙여서 구분할 수 있도록 함

--- src/data/api/menu.js
+++ src/data/api/menu.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import baseUrl from '../baseUrl';
 
-export const getMenuList = async () => {
+export const getMenuListApi = async () => {
   const response = await axios.get(`${baseUrl}/api/vl/menu`);
   return response.data;
 };
 
-export const getMenuById = async id => {
+export const getMenuByIdApi = async id => {
   const response = await axios.post(`${baseUrl}/api/vl/menu/${id}`);
   return response.data;
 };
