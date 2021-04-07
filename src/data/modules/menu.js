9b8163a0966b4498ef7a81ec7ac0ec1b19a15084
jihun1233 <leejihun6535@gmail.com>
2021-04-07 16:22:22 +0900

    [ADD] data/modules/menu.js 추가
    - 메뉴 리스트 및 메뉴 상세 조회를 위한 리듀서 파일 추가.
    
    - 액션 타입, 액션 생성 함수, state 초기값, 사가 정의, 통합 사가, 리듀서 정의중
    - 미완이며 마무리 될 시 msw 추가하여 MenuListView와 연동하여 확인해볼예정.

--- src/data/modules/menu.js
+++ src/data/modules/menu.js
@@ -1,1 +1,80 @@
 // import { delay, put, takeEvery, takeLatest } from 'redux-saga';
+
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { getMenuListApi, getMenuByIdApi } from '../api/menu';
+// 액션 타입
+
+// 메뉴 리스트 조회 / 성공 / 실패
+const GET_MENU_LIST = 'menu/GET_MENU_LIST';
+const GET_MENU_LIST_SUCCESS = 'menu/GET_MENU_LIST_SUCCESS';
+const GET_MENU_LIST_ERROR = 'menu/GET_MENU_LIST_ERROR';
+
+// 메뉴ID조회 / 성공 / 실패
+const GET_MENU_BY_ID = 'menu/GET_MENU_BY_ID';
+const GET_MENU_BY_ID_SUCCESS = 'menu/GET_MENU_BY_ID_SUCCESS';
+const GET_MENU_BY_ID_ERROR = 'menu/GET_MENU_BY_ID_ERROR';
+
+// 액션 생성 함수
+export const getMenuList = () => ({ type: GET_MENU_LIST });
+export const getMenuById = () => ({ type: GET_MENU_BY_ID });
+
+// state 초기값
+const initialState = {
+  menuList: [
+    {
+      child: [],
+      description: 'Menu Description',
+      menuId: 0,
+      menuOrder: 0,
+      nameEng: 'Menu Name in Eng',
+      nameKor: 'Menu Name in Kor'
+    }
+  ],
+  menu: {
+    child: [],
+    description: 'Menu Description',
+    menuId: 0,
+    menuOrder: 0,
+    nameEng: 'Menu Name in Eng',
+    nameKor: 'Menu Name in Kor'
+  }
+};
+
+// 사가 정의
+function* getMenuListSaga(action) {
+  try {
+    const res = yield call(getMenuListApi, action.payload);
+    yield put({ type: GET_MENU_LIST_SUCCESS, payload: res });
+  } catch (e) {
+    yield put({ type: GET_MENU_LIST_ERROR, error: true, payload: e });
+  }
+}
+function* getMenuByIdSaga(action) {
+  try {
+    const res = yield call(getMenuByIdApi, action.payload);
+    yield put({ type: GET_MENU_BY_ID_SUCCESS, payload: res });
+  } catch (e) {
+    yield put({ type: GET_MENU_BY_ID_ERROR, error: true, payload: e });
+  }
+}
+// menu의 사가들을 합친 통합 사가
+export function* menuSaga() {
+  yield takeLatest(GET_MENU_LIST, getMenuListSaga);
+  yield takeLatest(GET_MENU_BY_ID, getMenuByIdSaga);
+}
+
+// 리듀서 정의 export default
+export default function menu(state = initialState, action) {
+  switch (action.type) {
+    case GET_MENU_LIST:
+    case GET_MENU_LIST_SUCCESS:
+    case GET_MENU_LIST:
+      return;
+    case GET_MENU_BY_ID:
+    case GET_MENU_BY_ID:
+    case GET_MENU_BY_ID:
+      return;
+    default:
+      return state;
+  }
+}
