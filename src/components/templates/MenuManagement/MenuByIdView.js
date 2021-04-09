02560abeabaf0fcfb1a5f13c142982b3740ac4f5
jihun1233 <leejihun6535@gmail.com>
2021-04-09 20:49:26 +0900

    [UPDATE] 메뉴정보 상세조회화면 MenuByIdView 추가
    - 메뉴정보 상세조회화면 MenuByIdView 추가
    
    - 내부에 내용을 보여줄 테이블 또는 텍스트출력 컴포넌트가 필요함. 해당 module 제작필요
    - 이후 redux / saga / msw 적용하여 확인해볼 수 있도록 계획

---
+++ src/components/templates/MenuManagement/MenuByIdView.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import ReactRouterPropTypes from 'react-router-prop-types';
+// 메뉴 정보를 표시해줄 틀 module import
+/**
+ *  useSelect, useDispatch 사용
+ * menuById state를 select 하고, dispatch를 통해 getMenuById액션 호출.
+ * 이후 데이터를 가공하여 하위 모듈에 props로 전달.
+ * presentational component에서 출력.
+ *
+ * props에서 react-router의 match 속성을 받아서 이중 params로 url 마지막에 /:id로 들어온 부분을
+ * 받아서 action.payload에 담아서 dispatch.
+ *  */
+
+const MenuByIdView = ({ match }) => {
+  return (
+    <div>
+      <p>MenuByIdView {match.params ? match.params.id : ''}</p>
+    </div>
+  );
+};
+MenuByIdView.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired
+};
+
+export default MenuByIdView;
