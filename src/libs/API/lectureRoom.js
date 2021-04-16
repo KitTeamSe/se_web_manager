732b534d3728410caadfd69662c1abf8185090f0
hwanfront <kim015jh@gmail.com>
2021-04-16 21:48:05 +0900

    [UPDATE] 불필요한 파일 삭제 및 제외

--- src/libs/API/lectureRoom.js
+++
@@ -1,8 +0,0 @@
-import client from './client';
-
-export const getLectureRooms = () => client.get(`/api/v1/lecture-room`);
-export const getLectureRoom = id => client.get(`/api/v1/lecture-room/${id}`);
-export const addLectureRoom = () => client.post(`/api/v1/lecture-room`);
-export const updateLectureRoom = () => client.put(`/api/v1/lecture-room`);
-export const removeLectureRoom = id =>
-  client.delete(`/api/v1/lecture-room/${id}`);
