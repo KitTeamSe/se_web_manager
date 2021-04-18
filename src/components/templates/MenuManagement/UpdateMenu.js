9a93711a5c1cfae684add6744c84b0e01df339f1
jihun1233 <leejihun6535@gmail.com>
2021-04-19 02:04:07 +0900

    [UPDATE] MenuUpdateDialog 대신 MenuCreateDialog를 사용
    - value 값을 메뉴 정보 조회에서 받아온 값으로 지정하고, 수정하는 방식으로 사용
    
    - handleClose에 값을 json형태로 담아서 dispatch하는 것과 toggle을 실행하는 함수를 내려줌

--- src/components/templates/MenuManagement/UpdateMenu.js
+++ src/components/templates/MenuManagement/UpdateMenu.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import MenuUpdateDialog from '../../modules/MenuDialog/MenuUpdateDialog';
+import { useDispatch } from 'react-redux';
+import MenuCreateDialog from '../../modules/MenuDialog/MenuCreateDialog';
+import { updateMenu } from '../../../modules/menu';
 
-const UpdateMenu = ({ open, toggle }) => {
+const UpdateMenu = ({ open, toggle, menuData }) => {
+  const dispatch = useDispatch();
+
+  const [menuUpdateFormData, setMenuUpdateFormData] = useState({
+    menuOrder: menuData.menuOrder,
+    nameEng: menuData.nameEng,
+    nameKor: menuData.nameKor,
+    description: menuData.description,
+    parentId: menuData.parentId,
+    url: menuData.url
+  });
+
+  const items = [
+    { name: 'menuOrder', label: '순서', value: menuUpdateFormData.menuOrder },
+    { name: 'nameEng', label: '영어이름', value: menuUpdateFormData.nameEng },
+    { name: 'nameKor', label: '한글이름', value: menuUpdateFormData.nameKor },
+    {
+      name: 'description',
+      label: '설명',
+      value: menuUpdateFormData.description
+    },
+    { name: 'parentId', label: '상위메뉴', value: menuUpdateFormData.parentId },
+    { name: 'url', label: 'URL', value: menuUpdateFormData.url }
+  ];
+
+  const menuUpdateOnChange = e => {
+    const { value, name } = e.target;
+    setMenuUpdateFormData({ ...menuUpdateFormData, [name]: value });
+  };
+  const dispatchMenuUpdate = () => {
+    const jsonData = {};
+    items.forEach(item => {
+      jsonData[item.name] = item.value;
+    });
+    dispatch(updateMenu(jsonData));
+  };
+
+  const submitUpdateMenu = () => {
+    dispatchMenuUpdate();
+    toggle();
+  };
   return (
     <div>
-      <MenuUpdateDialog title="메뉴 수정" open={open} toggle={toggle} />
+      <MenuCreateDialog
+        title="메뉴 수정"
+        items={items}
+        open={open}
+        toggle={toggle}
+        handleClose={submitUpdateMenu}
+        onChange={menuUpdateOnChange}
+      />
     </div>
   );
 };
 
 UpdateMenu.propTypes = {
   open: PropTypes.bool.isRequired,
-  toggle: PropTypes.func.isRequired
+  toggle: PropTypes.func.isRequired,
+  menuData: PropTypes.objectOf(PropTypes.string).isRequired
 };
 export default UpdateMenu;
