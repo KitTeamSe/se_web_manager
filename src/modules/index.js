be83809ef70db61c6730835aec672ae3905c855c
hwanfront <kim015jh@gmail.com>
2021-04-19 23:53:03 +0900

    [DELETE] 현재 사용하지 않는 redux 데이터 삭제

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,16 +3,14 @@ import { all } from 'redux-saga/effects';
 
 import loading from './loading';
 import auth, { authSaga } from './auth';
-import menu, { menuSaga } from './menu';
 
 const rootReducer = combineReducers({
   loading,
-  auth,
-  menu
+  auth
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), menuSaga()]);
+  yield all([authSaga()]);
 }
 
 export default rootReducer;
